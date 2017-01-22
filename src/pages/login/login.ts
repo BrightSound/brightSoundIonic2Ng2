014b4ac09fb645c380087d675838de1a972b88d1
Dmitry Masukevich <masukevich@gmail.com>
2017-01-22 14:35:50 +0300

    Reactive form for login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder }
+  from '@angular/forms';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  registerCredentials = {email: '', password: ''};
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      "email":["", Validators.required],
+      "password":
+        [ "",
+          Validators.compose(
+            [
+              Validators.required,
+              Validators.minLength(8),
+              Validators.maxLength(20)
+            ]
+          )
+        ]
+    });
+  }
 
   public login() {
-    alert([this.registerCredentials.email, this.registerCredentials.password]);
+    console.log(this.form.value);
   }
 
 }
